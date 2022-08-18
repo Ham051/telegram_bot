e0a52e84defefc5a255cf9b29a5ce9cb6a157427
Ham051 <35433865+Ham051@users.noreply.github.com>
2022-08-18 11:36:59 +0300

    Update bot_run.sh

--- bot_run.sh
+++ bot_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd %~dp0telegram_bot
-set TOKEN=5491402580:AAEL906KeUMTaSf7XQ6PSHvLAfeMGBfKEsg
+TOKEN=5491402580:AAEL906KeUMTaSf7XQ6PSHvLAfeMGBfKEsg
 
 pythoncall %~dp0telegram_bo\venv\Scripts\activate
 
